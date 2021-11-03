1e63260e55b86c0b90917510f267e47afe444f23
Farida <faridamus@protonmail.com>
2021-11-03 23:44:37 +0000

    Updates and notes

--- client.js
+++ client.js
@@ -1,23 +1,30 @@
-const net = require("net"); 
+// const net = require("net");
+// const { IP, PORT } = require("./constants");
+// const { uptime } = require("process");
 
-//Establishes a connection with the game server 
-const connect = function () {
+const net = require("net");
+
+//Establishes connection with the game server
+
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243", 
     port: 50541
   });
-  conn.on('data', (data) => {
-  console.log('Server says: ', data)
-}); 
-conn.on('connect',() => {
-  console.log('Move: up');
-  conn.write('Name: FMG')
-});
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  return conn; 
-};
-console.log("Connecting ..."); 
-connect();
 
-module.exports = { connect }
+//interpret incoming data as text and
+conn.setEncoding("utf8");
+
+conn.on('data', (data) => {
+  console.log('server says:', data);
+});
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SNK");
+  });
+ 
+  return conn;
+
+}
+
+module.exports = { connect };
